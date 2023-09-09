cfd5dfa1a3d8190c9fc232bf4bbcf23c761277b2
Legit Killer <91422151+LegitKillerOP@users.noreply.github.com>
2023-09-09 14:10:45 +0530

    Initial commit

---
+++ src/main/java/me/legit/rtp/RTP.java
@@ -0,0 +1,17 @@
+package me.legit.rtp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RTP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
